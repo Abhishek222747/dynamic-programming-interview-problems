6f5b71e1cf5728f597b98ba4168eddf17c8a7442
Abhishek Kumar <140600879+Abhishek222747@users.noreply.github.com>
2025-06-29 01:48:16 -0700

    Add files via upload

---
+++ Nth Fibonacci Number.cpp
@@ -0,0 +1,29 @@
+Given a non-negative integer n, your task is to find the nth Fibonacci number.
+
+The Fibonacci sequence is a sequence where the next term is the sum of the previous two terms.
+The first two terms of the Fibonacci sequence are 0 followed by 1. 
+The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
+The Fibonacci sequence is defined as follows:
+F(0) = 0
+F(1) = 1
+F(n) = F(n - 1) + F(n - 2) for n > 1
+
+
+// User function Template for C++
+class Solution {
+  private:
+    int solve(int n, vector<int>& dp){
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+  public:
+    int nthFibonacci(int n) {
+        // code here
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
